8cadac372ba6df12645a452eebe031cefa201781
Ashish <agnihotri.ashish@gmail.com>
2023-01-30 00:32:26 -0800

    trying

---
+++ bin/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='getquote',
+    version='0.0.6',
+    author='Ashish Agnihotri',
+    author_email='your.email@example.com',
+    description='A sample Python script',
+    install_requires=[
+        'requests'
+    ],
+    py_modules=['getquote'],
+)
