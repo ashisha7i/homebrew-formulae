3ef5434c25ae44747bc0d3828f79ed308c35e3cd
Ashish <agnihotri.ashish@gmail.com>
2023-01-29 23:14:19 -0800

    Updated .gitignore and deleted unneeded files

---
+++ bin/getquote.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    quote = data.get("quoteText")
+    print(quote)
+else:
+    print("Failed to get quote")
