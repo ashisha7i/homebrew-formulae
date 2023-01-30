bcf4324edeff1dbc92ccde7a449a812c2fefa251
Ashish <agnihotri.ashish@gmail.com>
2023-01-30 02:04:08 -0800

    update

--- getquote.py
+++ getquote.py
@@ -1,11 +1,15 @@
 import requests
 
-url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
-response = requests.get(url)
+def request_quote():
+    url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
+    response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
-    quote = data.get("quoteText")
-    print(quote)
-else:
-    print("Failed to get quote")
+    if response.status_code == 200:
+        data = response.json()
+        quote = data.get("quoteText")
+        print(quote)
+    else:
+        print("Failed to get quote")
+
+def main():
+    request_quote()
