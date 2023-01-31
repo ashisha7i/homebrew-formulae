b8b2591ced8dfc3e12965b3021f9c253130d8750
Ashish <agnihotri.ashish@gmail.com>
2023-01-30 16:59:39 -0800

    Exception handled for JSON parsing

--- getquote.py
+++ getquote.py
@@ -1,3 +1,5 @@
+import json
+import time
 import requests
 
 def request_quote():
@@ -5,11 +7,17 @@ def request_quote():
     response = requests.get(url)
 
     if response.status_code == 200:
-        data = response.json()
-        quote = data.get("quoteText")
-        print(quote)
+        try:
+            data = response.json()
+            quote = data.get("quoteText")
+            print(quote)
+        except json.decoder.JSONDecodeError:
+            time.sleep(0.5)
+            request_quote()
     else:
         print("Failed to get quote")
 
 def main():
     request_quote()
+
+main()
